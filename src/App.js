6e0b534be7194d77a4bbc9bc3080fb020a4fe0c8
DEEPAKPETWAR <148950560+DEEPAKPETWAR@users.noreply.github.com>
2024-04-02 13:13:32 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+//import AllSoulUpPeers1 from "./Components/AllSoulUpPeers/AllSoulUpPeers1";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-  <Header/>
-  <Navbar/>
+   <Header/>
+
+  
+  
     </div>
   );
 }
