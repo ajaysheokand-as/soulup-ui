48c5fbff0e203d179b3161ff860ca443ff4df2cf
DEEPAKPETWAR <148950560+DEEPAKPETWAR@users.noreply.github.com>
2024-04-08 13:15:49 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Header from "./Components/Header/Header";
-import MobileNavbar from "./Components/Navbar/MobileNavbar";
-import Navbar from "./Components/Navbar/Navbar";
-import Center from "./Components/Center/Center";
-import ConnectRelavant from "./Components/ConnectRelavant/ConnectRelavant";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import Peerinfo from "./Components/Peerinfo/Peerinfo";
+// import Header from "./Components/Header/Header";
+// import MobileNavbar from "./Components/Navbar/MobileNavbar";
+// import Navbar from "./Components/Navbar/Navbar";
+// import Center from "./Components/Center/Center";
+// import ConnectRelavant from "./Components/ConnectRelavant/ConnectRelavant";
+// import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+// import Peerinfo from "./Components/Peerinfo/Peerinfo";
+import PopUp from "./Components/PopUp/PopUp";
 function App() {
 
   return (
@@ -13,15 +14,16 @@ function App() {
       <MobileNavbar/>
       <Header/>
       <AllSoulUpPeers1/>
+      
       <AllSoulUpPeer3/>
       <Center/> */}
-
-      <Router>
+<PopUp/>
+      {/* <Router>
         <Navbar/> 
         <MobileNavbar/>
         <Routes>
           
-          <Route
+          {/* <Route
             exact 
             path="/"
             element={<AllSoulUpPeer2 data_from_parent={data_from_child}/>}
@@ -30,10 +32,10 @@ function App() {
             exact 
             path="/peerinfo"
             element={<Peerinfo item={data}/>}
-          ></Route>
+          ></Route> }
         </Routes>
       </Router>
-      
+       */}
       
       
 
---
+++ src/Components/PopUp/PopUp.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import "./PopUp.css";
+import { useState } from 'react';
+const PopUp = () => {
+
+    const[action,setAction] = useState("Log In");
+  return (
+    <div className='Box'>
+        <div className=' H_3'>
+            <div className='text'>{action}</div>
+            <div className="underline"></div>
+            <button className='delete'>X</button>
+
+            
+        </div>
+      <div className="inputs">
+        {action==="Log In"?<div></div>:        <div className='input'>
+          <h3> User</h3> l
+            <input type="text" placeholder='Enter User Name'/>
+
+            </div>}
+        
+            
+        <div className='input'>
+       <h3>Email</h3>
+            <input type="email" placeholder='Enter your Email'/>
+
+            </div>
+        <div className='input'>
+           <h3>Password </h3>
+            <input type="password" placeholder='Enter your Password'/>
+
+            </div>
+      </div>
+      {action==="Sign Up"? <div></div>:<div className="forgot-password">Lost Password?<span>Click Here!</span></div>
+}
+
+
+
+
+      <div className="submit-container">
+        <div className={action ==="Log In"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up </div>
+        <div className={action ==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Log In")}}>Log In </div>
+
+      </div>
+     
+    </div>
+  )
+}
+
+export default PopUp
